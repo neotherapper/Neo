f6f641650d497ee6a6aa589c6750d5a575c5ce0b
Georgios Pilitsoglou <george.pilitsoglou@gmail.com>
2020-03-21 22:57:04 +0000

    fixed models

--- src/components/neo-tournament-category-list-item-view/tournament-category-list.model.ts
+++
@@ -1,5 +0,0 @@
-import { TournamentCategoryListItemI } from "../neo-tournament-category-list-view/tournament-category-list-item.model";
-
-export interface TournamentCategoryList {
-  categories: TournamentCategoryListItemI[]
-}
